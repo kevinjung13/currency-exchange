fcf5af3a0b37f498a8742513cad92bd12850d0f2
“Kevin <kevinjung13@@gmail.comgit config --global user.name Kevin Junggit config --global user.email kevinjung13@@gmail.com>
2023-03-03 10:15:06 +0700

    second commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,27 +4,33 @@ export default function Footer() {
 
   return (
     <footer>
-      <div className='left'> {/* Left side of footer */} 
-      <ul> {/* list of icons */} 
-          <li> {/* Linkedin */} 
+      {/* Left side of footer */} 
+      <div className='left'> 
+      {/* list of icons */} 
+        <ul> 
+          {/* Linkedin */} 
+          <li> 
             <a className="bounce" target="_blank" href="https://www.linkedin.com/in/kevin-hwisung-jung-11374a6a/">
               <i className="fa-brands fa-linkedin fa-lg"></i>
             </a>
           </li>
-          <li> {/* Twitter */} 
+          {/* Twitter */}
+          <li>  
             <a className="bounce" target="_blank" href="https://twitter.com/kevinhjung93">
               <i className="fa-brands fa-twitter fa-lg"></i>
             </a>
           </li>
-          <li> {/* Email */} 
+          {/* Email */} 
+          <li> 
             <a className="bounce" target="_blank" href="mailto:kevinjung13@gmail.com">
               <i className="fa-solid fa-envelope fa-lg"></i>
             </a>
           </li>
         </ul>
       </div>
-      <div className='right'> {/* Right side of footer */} 
-        <p>Created & Designed by Kevin Jung</p>
+      {/* Right side of footer */} 
+      <div className='right'> 
+        <p className='creator'>Created & Designed by Kevin Jung</p>
       </div>
   </footer>
 )
